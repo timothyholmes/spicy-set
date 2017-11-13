e88981f65dc40d25341d734bc0e2f01030c3f81d
Tim Holmes <timothy.holmes@homes.com>
2017-11-13 15:30:08 -0500

    Created proto methods for interacting with objects based on value instead of reference in sets

---
+++ README.md
@@ -0,0 +1,32 @@
+# Spicy Set 🔥
+
+Creates `Set` methods for interacting with objects based on content instead of reference.
+
+## Usage
+
+```javascript
+
+const SpicySet = require('spicy-set');
+
+const vanillaSet = new Set();
+const spicySet = SpicySet();
+
+vanillaSet.add({ hello: 'world' });
+spicySet.add({ hello: 'world' });
+
+vanillaSet.has({ hello: 'world' }); // output: false
+spicySet.hasObject({ hello: 'world' }); // output: true
+
+vanillaSet.add({ hello: 'world' }); // Adds object, Set is { {hello: 'world'}, {hello: 'world'} }
+spicySet.addObject({ hello: 'world' }); // Doesn't add object, Set is { {hello: 'world'} }
+```
+
+## Scripts
+
+- unit tests: `npm test`
+- html test coverage: `npm run html`
+- build and view docs: `npm run docs`
+
+## Help
+
+Why? [JavaScript evaluates objects by reference and not value.](https://stackoverflow.com/questions/6605640/javascript-by-reference-vs-by-value)
---
+++ test/index_test.js
@@ -0,0 +1,110 @@
+
+const _ = require('lodash');
+const Lab = require('lab');
+const should = require('should');
+const SpicySet = require('../lib/index');
+
+exports.lab = Lab.script();
+const lab = exports.lab;
+const describe = lab.describe;
+const it = lab.it;
+
+// test data
+const futureSoloAlbums = {
+  beastMode: true,
+  fiftySixNights: true,
+  monster: true,
+  WATTBA: false,
+  superSlimey: false
+};
+
+const youngThugSoloAlbums = {
+  slimeSeasonOne: true,
+  barterSix: true,
+  superSlimey: false,
+  easyBreezy: true
+};
+
+describe('Spicy Set', () => {
+  describe('Constructor', () => {
+    it('should return a set when called with no arguments', (done) => {
+      const setWithoutArguments = SpicySet();
+
+      should(setWithoutArguments).be.type('object');
+
+      done();
+    });
+    it('should return a set when called with an iterable', (done) => {
+      const setWithIterable = SpicySet([1, 2, 3]);
+
+      should(setWithIterable).be.type('object');
+      should(setWithIterable.has(1)).equal(true);
+      should(setWithIterable.has(2)).equal(true);
+      should(setWithIterable.has(3)).equal(true);
+
+      done();
+    });
+  });
+  describe('hasObject', () => {
+    it('should return true if object with same content is in set', (done) => {
+      const set = SpicySet();
+
+      set.add(_.cloneDeep(futureSoloAlbums));
+
+      should(set.hasObject(futureSoloAlbums)).equal(true);
+      should(set.has(futureSoloAlbums)).equal(false);
+
+      done();
+    });
+    it('should return false if object is not in set', (done) => {
+      const set = SpicySet();
+
+      set.add(_.cloneDeep(futureSoloAlbums));
+
+      should(set.hasObject(futureSoloAlbums)).equal(true);
+      should(set.has(futureSoloAlbums)).equal(false);
+      should(set.hasObject(youngThugSoloAlbums)).equal(false);
+      should(set.has(youngThugSoloAlbums)).equal(false);
+
+      done();
+    });
+  });
+  describe('addObject', () => {
+    it('should add the object if not present and return the set', (done) => {
+      const set = SpicySet();
+
+      set.add(_.cloneDeep(futureSoloAlbums));
+
+      // check the inventory before we add to it
+      should(set.hasObject(futureSoloAlbums)).equal(true);
+      should(set.has(futureSoloAlbums)).equal(false);
+      should(set.hasObject(youngThugSoloAlbums)).equal(false);
+      should(set.has(youngThugSoloAlbums)).equal(false);
+
+      // add the object to the set and check the inventory
+      should(set.addObject(_.cloneDeep(youngThugSoloAlbums))).equal(set);
+      should(set.hasObject(youngThugSoloAlbums)).equal(true);
+      should(set.has(youngThugSoloAlbums)).equal(false);
+
+      done();
+    });
+    it('should not add the object if already present and return the set', (done) => {
+      const set = SpicySet();
+
+      set.add(_.cloneDeep(futureSoloAlbums));
+      set.add(_.cloneDeep(youngThugSoloAlbums));
+
+      should(set.hasObject(futureSoloAlbums)).equal(true);
+      should(set.has(futureSoloAlbums)).equal(false);
+      should(set.hasObject(youngThugSoloAlbums)).equal(true);
+      should(set.has(youngThugSoloAlbums)).equal(false);
+
+      // add the object to the set and check the inventory
+      should(set.addObject(_.cloneDeep(youngThugSoloAlbums))).equal(set);
+      should(set.hasObject(youngThugSoloAlbums)).equal(true);
+      should(set.has(youngThugSoloAlbums)).equal(false);
+
+      done();
+    });
+  });
+});
